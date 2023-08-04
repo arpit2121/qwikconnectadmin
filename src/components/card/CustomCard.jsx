29978cd85e34f99ea74769865b0ed5f384adc746
arpit2121 <arpitsingh9892@gmail.com>
2023-08-05 01:37:22 +0530

    cahnges

--- src/components/card/CustomCard.jsx
+++ src/components/card/CustomCard.jsx
@@ -4,22 +4,7 @@ import { Card, CardContent, CardHeader, useMediaQuery } from '@mui/material';
 import useResponsiveStyles from '../../utils/MediaQuery';
 
 export const CustomCard = styled(Card)(({ theme, responsive }) => ({
-  // margin: '1rem',
   padding:'1rem',
-  height:'100%',
-  // backgroundColor:'#fff'
-  // display:'flex',
-  // flexDirection:'column',
-  // alignItems:'center'
-  // [theme.breakpoints.up('sm')]: {
-  //   maxWidth: responsive.isMobile ? '100%' : '75%',
-  // },
-  // [theme.breakpoints.up('md')]: {
-  //   maxWidth: responsive.isTablet ? '100%' : '50%',
-  // },
-  // [theme.breakpoints.up('lg')]: {
-  //   maxWidth: responsive.isDesktop ? '100%' : '33.33%',
-  // },
 }));
 
 const CustomCardComponent = ({ title, content }) => {
