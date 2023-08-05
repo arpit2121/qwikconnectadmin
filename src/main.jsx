91f04d9b8706298c34657012ed2e55e9bcfb8f3d
arpit2121 <arpitsingh9892@gmail.com>
2023-08-06 03:06:52 +0530

    theme added

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,22 @@ import { BrowserRouter } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import { Provider } from 'react-redux'
 import store from './store.js'
+// import { ThemeProvider } from '@mui/material'
+import { darkspacetheme } from './theme/theme.js'
+import { ThemeProvider } from 'styled-components'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
-    <GoogleOAuthProvider clientId='802265504166-7q42g3j4pvcs7gsmmr1u70de4n7m79il.apps.googleusercontent.com'>
+    {/* <GoogleOAuthProvider clientId='802265504166-7q42g3j4pvcs7gsmmr1u70de4n7m79il.apps.googleusercontent.com'> */}
+    {/* <ThemeProvider theme={darkspacetheme}> */}
+    <ThemeProvider theme={darkspacetheme}>
     <BrowserRouter>
     <App />
     </BrowserRouter>
-    </GoogleOAuthProvider> 
+    </ThemeProvider>
+    {/* </ThemeProvider> */}
+    {/* </GoogleOAuthProvider>  */}
     </Provider>
     
 )
---
+++ src/theme/colors.pallete.js
@@ -0,0 +1,28 @@
+export const colors ={
+    primary:'#202020',//black
+    secondary: '#3D3ACE',//blue
+    teritary: '#F845FC',//purple
+    darkpurple: '#23209F',
+    grey: '#AAAAAA',
+    lightgrey: '#C9C8D3',
+    disabled: '#C5C0DB',
+    white: '#FFFFFF',
+    error: '#F93232',
+    success: '#439F6E',
+    warning: '#FFB82E',
+    strokegrey: '#BDBCDB',
+    bgdisabled: '#F4F3FE',
+    bghover: '#F6F5FF',
+    bgclick: '#E2E1FF',
+    strokeerror:'#FFD8D8',
+    loading: '#E8E6F8',
+    bgvalidation:'#FFFBF4',
+    strokevalidation: '#FFEAC1',
+    textdisabled: '#A5A5A7',
+    fieldtitle: '#9D99AC',
+    blue: '#605DEC',
+    fieldform: '#F7F7FD',
+    fieldtext: '#212121',
+    bgerror: '#FFF5F5',
+}
+
