12c727ebae8e6023ea7af936582bbd917ae7ed4a
arpit2121 <arpitsingh9892@gmail.com>
2023-08-08 13:10:05 +0530

    user plus icon removed

--- src/components/icons/UserPlusIcon.jsx
+++ src/components/icons/UserPlusIcon.jsx
@@ -1,58 +1,45 @@
 import React from "react";
 
-function UserPlusIcon({ height = 32, width = 32, className,style }) {
+const UserIcon = ({ height = 32, width = 32, className,style }) => {
   return (
     <svg
     style={style}
     width={height}
     height={width}
     className={className}
-      viewBox="0 0 33 33"
+      viewBox="0 0 32 32"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_184_5101)">
-        <path
-          d="M21.6134 28.2422V25.5755C21.6134 24.161 21.0515 22.8045 20.0513 21.8043C19.0511 20.8041 17.6945 20.2422 16.28 20.2422H6.94669C5.53221 20.2422 4.17565 20.8041 3.17546 21.8043C2.17526 22.8045 1.61336 24.161 1.61336 25.5755V28.2422"
-          stroke="#AA55FF"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M11.6134 14.9089C14.5589 14.9089 16.9467 12.521 16.9467 9.57552C16.9467 6.63 14.5589 4.24219 11.6134 4.24219C8.66784 4.24219 6.28003 6.63 6.28003 9.57552C6.28003 12.521 8.66784 14.9089 11.6134 14.9089Z"
-          stroke="#AA55FF"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M26.9467 10.9089V18.9089"
-          stroke="#AA55FF"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-        <path
-          d="M30.9467 14.9089H22.9467"
-          stroke="#AA55FF"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      </g>
-      <defs>
-        <clipPath id="clip0_184_5101">
-          <rect
-            width="32"
-            height="32"
-            fill="white"
-            transform="translate(0.280029 0.242188)"
-          />
-        </clipPath>
-      </defs>
+      <path
+        d="M22.6668 28V25.3333C22.6668 23.9188 22.1049 22.5623 21.1047 21.5621C20.1045 20.5619 18.748 20 17.3335 20H6.66683C5.25234 20 3.89579 20.5619 2.89559 21.5621C1.8954 22.5623 1.3335 23.9188 1.3335 25.3333V28"
+        stroke="#4787F3"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokelinejoin="round"
+      />
+      <path
+        d="M11.9998 14.6667C14.9454 14.6667 17.3332 12.2789 17.3332 9.33333C17.3332 6.38781 14.9454 4 11.9998 4C9.05432 4 6.6665 6.38781 6.6665 9.33333C6.6665 12.2789 9.05432 14.6667 11.9998 14.6667Z"
+        stroke="#4787F3"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokelinejoin="round"
+      />
+      <path
+        d="M30.6665 28V25.3333C30.6656 24.1516 30.2723 23.0037 29.5483 22.0698C28.8243 21.1358 27.8107 20.4688 26.6665 20.1733"
+        stroke="#4787F3"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokelinejoin="round"
+      />
+      <path
+        d="M21.3335 4.17334C22.4807 4.46707 23.4975 5.13427 24.2237 6.06975C24.9498 7.00523 25.344 8.15578 25.344 9.34001C25.344 10.5242 24.9498 11.6748 24.2237 12.6103C23.4975 13.5457 22.4807 14.2129 21.3335 14.5067"
+        stroke="#4787F3"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokelinejoin="round"
+      />
     </svg>
   );
-}
-
-export default UserPlusIcon;
+};
+export default UserIcon;
